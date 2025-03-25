a9bc7122b9c6b90f98eff814b836c5b04dd58935
Ucodify <hasasamee@gmail.com>
2025-03-25 14:00:10 -0700

    first-app

---
+++ my-project/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+import Example from "./components/cupe";
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Example />
+    </>
+  );
+}
